15249e03e5eef08bfeb0c81742323f7c1e08accb
MarkSabbadini <marcosabbadini99@gmail.com>
2024-05-29 21:34:53 +0200

    Laravel first steps

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [FrontController::class, 'getHome'])->name('home'); // V
